4b02ec7f329fc7af4a1703da3d54a9b5973e6339
Dimitar Bosnev <dimitar.bosnew@gmail.com>
2024-05-08 12:26:38 +0200

    VR-ControllerHands

---
+++ Assets/Scripts/HandAnimationScript.cs
@@ -0,0 +1,24 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class HandAnimationScript : MonoBehaviour
+{
+    [SerializeField] private InputActionProperty pinchProperty;
+    [SerializeField] private InputActionProperty gripProperty;
+    private Animator handAnimatior;
+    // Start is called before the first frame update
+    void Start()
+    {
+        handAnimatior = gameObject.GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float triggerValue = pinchProperty.action.ReadValue<float>();
+        handAnimatior.SetFloat("Trigger", triggerValue);
+        Debug.Log("Trigger Value: " + triggerValue);
+        float gripValue = gripProperty.action.ReadValue<float>();
+        handAnimatior.SetFloat("Grip",gripValue);
+    }
+}
