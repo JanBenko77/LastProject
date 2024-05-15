5238a2c08b5b58444aa398aeaec6f717f28e64e3
Dimitar Bosnev <dimitar.bosnew@gmail.com>
2024-05-15 11:02:29 +0200

    Project Restructure

---
+++ Assets/Scripts/Objective Scripts/ObjectiveItem.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+
+public class ObjectiveItem : MonoBehaviour
+{
+    [SerializeField] private ObjectiveType _objectiveType;
+    public ObjectiveType ObjectiveType{ get{ return _objectiveType; } }
+    // Start is called before the first frame update
+    void Start(){
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/Objective Scripts/ObjectiveManager.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+
+public enum ObjectiveType{
+    None = 0,
+    Ball = 1,
+}
+public class ObjectiveManager : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
