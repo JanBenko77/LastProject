a1c3a1cc7bf13d699fcffabcca62e8e152b16d22
Dimitar Bosnev <dimitar.bosnew@gmail.com>
2024-06-13 15:35:00 +0200

    XR rig fix and FPS counter Improved

--- Assets/Scripts/FPSCounter.cs
+++ Assets/Scripts/FPSCounter.cs
@@ -2,6 +2,8 @@ using UnityEngine;
 using TMPro;
 public class FPSCounter : MonoBehaviour
 {
+    int timeout;
+    int maxTime = 10;
     [SerializeField] private TextMeshProUGUI _fpsText;
     // Start is called before the first frame update
     void Start()
@@ -13,6 +15,17 @@ public class FPSCounter : MonoBehaviour
     // Update is called once per frames
     void Update()
     {
-        _fpsText.text ="FPS: " + (1f/Time.unscaledDeltaTime).ToString();
+        switch (timeout)
+        {
+            case 0:{
+                _fpsText.text ="FPS: " + Mathf.Floor(1f/Time.unscaledDeltaTime).ToString();
+                timeout = maxTime;
+            }
+            break;
+
+            default:
+                timeout--;
+            break;
+        }
     }
 }
