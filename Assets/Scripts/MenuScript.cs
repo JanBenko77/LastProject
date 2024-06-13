8713c2eb4a205bd1ebfb35256e31400c89a34f76
Dimitar Bosnev <dimitar.bosnew@gmail.com>
2024-06-13 16:29:58 +0200

    HandUI Changes

--- Assets/Scripts/MenuScript.cs
+++ Assets/Scripts/MenuScript.cs
@@ -2,6 +2,10 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 public class MenuScript : MonoBehaviour
 {
+    public GrassComputeScript grass;
+    void Start(){
+        grass = FindAnyObjectByType<GrassComputeScript>();
+    }
     public void OnHover(){
         Debug.Log("Hover");
     }
