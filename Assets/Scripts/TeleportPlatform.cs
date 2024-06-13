0a0ccb65ca5f602ec4df6b8118f07b19cc9a8d48
Jan Benko <jan.benko777@gmail.com>
2024-06-14 00:01:04 +0200

    Fading + teleporting

--- Assets/Scripts/TeleportPlatform.cs
+++ Assets/Scripts/TeleportPlatform.cs
@@ -5,12 +5,29 @@ using UnityEngine;
 public class TeleportPlatform : MonoBehaviour
 {
     public Transform destination;
+    private Transform player;
+
+    private void OnEnable()
+    {
+        EventBus<OnAnimationComplete>.OnEvent += Teleport;
+    }
+
+    private void OnDisable()
+    {
+        EventBus<OnAnimationComplete>.OnEvent -= Teleport;
+    }
 
     private void OnTriggerEnter(Collider other)
     {
         if(other.gameObject.CompareTag("Player"))
         {
-            other.gameObject.transform.position = destination.position + new Vector3(0, 1, 0);
+            player = other.gameObject.transform;
+            EventBus<OnTeleporterEntered>.Invoke(new OnTeleporterEntered());
         }
+    }
+
+    private void Teleport(OnAnimationComplete pEvent)
+    {
+        player.position = destination.position + new Vector3(0, 1, 0);
     }
 }
