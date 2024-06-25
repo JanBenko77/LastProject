a6143eded1bc38805632b3849b6f0e1ff16af023
Dimitar Bosnev <dimitar.bosnew@gmail.com>
2024-06-25 14:03:44 +0200

    Implemented scene switching

--- Assets/Scripts/Objective Scripts/ObjectiveItem.cs
+++ Assets/Scripts/Objective Scripts/ObjectiveItem.cs
@@ -5,29 +5,16 @@ using UnityEngine;
 public class ObjectiveItem : MonoBehaviour
 {
     [SerializeField] private ObjectiveType _objectiveType;
-    private GameObject player;
     public ObjectiveType ObjectiveType{ get{ return _objectiveType; } set { _objectiveType = value; } }
     // Start is called before the first frame update
     void Start(){
         EventBus<OnObjectiveComplete>.OnEvent += OnItemDelivered;
         EventBus<OnObjectiveActivated>.OnEvent += OnItemActivated;
         gameObject.layer = LayerMask.NameToLayer("Default");
-        player = FindObjectOfType<XROrigin>().gameObject;
     }
 
 
     // Update is called once per frame
-    void Update()
-    {
-        Physics.Raycast(transform.position, player.transform.position - transform.position,out RaycastHit hit,5);
-        if(hit.rigidbody.gameObject == player){
-            gameObject.SetLayerRecursively(LayerMask.NameToLayer("Interacting"));
-        }
-        else{
-            gameObject.SetLayerRecursively(LayerMask.NameToLayer("Objective"));
-        }
-    }
-
     void OnItemDelivered(OnObjectiveComplete pEvent){
         if(pEvent.item == this){
             Debug.Log("Item Delivered");
